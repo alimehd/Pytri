0f485065f60fc8c56bda63316bcfd2bf6706432b
alimehd <72367342+alimehd@users.noreply.github.com>
2020-12-22 14:17:21 -0500

    Add files via upload

---
+++ Annotations/rename_file.py
@@ -0,0 +1,15 @@
+import os
+import glob
+
+def rename(dir, pattern, titlePattern):
+    os.chdir(dir)
+    for index, oldfile in enumerate(glob.glob("*.jpg"), start=1):
+        newfile = '{}.jpg'.format(index)
+        os.rename(oldfile, newfile)
+    for pathAndFilename in glob.iglob(os.path.join(dir, pattern)):
+        title, ext = os.path.splitext(os.path.basename(pathAndFilename))
+        os.rename(pathAndFilename,
+                  os.path.join(dir, titlePattern % title + ext))
+
+# rename(r'directory path', r'*.jpg', r'ResearcherLastName_%s')
+rename(r'/Users/alimehdi/Desktop/Pytri/Tito_data1', r'*.jpg', r'Titorenko_%s')
